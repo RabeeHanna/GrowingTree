c3392fa10bb2ac37b7dc0b35f76acc913907f6c3
Rabee Hanna <rabee.hanna@mail.utoronto.ca>
2016-06-22 19:11:03 -0400

    Implemented new functions
    DrawAngleLine(angle, length):
    
    Draws a line from the current position with a length of 'length' at an
    angle to the horizontal of 'angle'.
    
    Update():
    Draws the strokes to the canvas

--- script.js
+++ script.js
@@ -18,25 +18,31 @@ class Client {
 			this.y = y;
 		}
 		this.setpos(x/2, y);
+		//this.setpos(200,300);
 
-		//Draws a line from current position to point (x,y)
+		//Draws a line from current position to point (x,y) 
 		var drawLineTo = function(x,y) {
 			self.ctx.lineTo(x,y);
-			self.ctx.stroke();
+			self.x = x;
+			self.y = y;
 		}
-		
-		//Draws a line with length len and angle angle
+
 		this.drawLineAngle = function(angle, len) {
-			drawLineTo(len * Math.cos(angle), len * Math.sin(angle))
+			drawLineTo(len*Math.cos(-angle) + this.x,len*Math.sin(-angle) + this.y);
+		}
+
+		//Add the drawing to the canvas
+		this.update = function() {
+			this.ctx.stroke();
 		}
 	}
-
-	
-
-
 }
 
+var PIOVER180 = (Math.PI / 180);
 
+function toRadians(angle) {
+	return angle * PIOVER180;
+}
 
 function setpos(x, y) {
 
@@ -44,6 +50,8 @@ function setpos(x, y) {
 
 window.onload = function() {
 	var client = new Client(document.getElementById("myCanvas"), 600, 600);
-	client.drawLineAngle(45, 10);
+	client.drawLineAngle(toRadians(45), 50);
+	client.drawLineAngle(toRadians(80), 50);
+	client.update();
 	console.log(client)
 }
